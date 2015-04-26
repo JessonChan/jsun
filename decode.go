538f9db66951a764e20b6c08d6bf9aa8027bd797
JessonChan <chinachendongpo@gmail.com>
2015-04-26 16:57:10 +0800

    rename package

--- decode.go
+++ decode.go
@@ -5,7 +5,7 @@
 // Represents JSON data structure using native Go types: booleans, floats,
 // strings, arrays, and maps.
 
-package jsonx
+package jsun
 
 import (
 	"bytes"
--- encode.go
+++ encode.go
@@ -8,7 +8,7 @@
 //
 // See "JSON and Go" for an introduction to this package:
 // http://golang.org/doc/articles/json_and_go.html
-package jsonx
+package jsun
 
 import (
 	"bytes"
